ce44f649b738ef36924a0b8987da15a985221a38
Alex Burner <alexb@extrahop.com>
2017-05-04 17:15:15 -0700

    sending tick & time

--- src/d3-force/worker.js
+++ src/d3-force/worker.js
@@ -1,16 +1,8 @@
 import * as d3_force from 'd3-force';
 
-let TICK_LIMIT = 1000;
-let TICK_BATCH = 10;
-
-let charge;
-let link;
-let x;
-let y;
 let simulation;
 let nodes;
 let links;
-let subject;
 
 const init = (args) => {
     nodes = args.nodes;
@@ -18,12 +10,12 @@ const init = (args) => {
     simulation = d3_force.forceSimulation();
     simulation.nodes(nodes);
     simulation.stop();
-    charge = d3_force.forceManyBody();
-    link = d3_force.forceLink(links);
+    const charge = d3_force.forceManyBody();
+    const link = d3_force.forceLink(links);
     link.distance(30);
     link.strength(1);
-    x = d3_force.forceX();
-    y = d3_force.forceY();
+    const x = d3_force.forceX();
+    const y = d3_force.forceY();
     simulation.force('charge', charge);
     simulation.force('link', link);
     simulation.force('x', x);
@@ -33,17 +25,25 @@ const init = (args) => {
 self.addEventListener('message', (e) => {
     switch (e.data.type) {
         case 'init': {
+            // initialize simulation
             init({
                 nodes: e.data.nodes,
                 links: e.data.links,
             });
-            for (let i = 0; i < e.data.nodes.length; i++) {
+            // run simulation
+            const batch = 10;
+            const limit = e.data.nodes.length;
+            const start = Date.now();
+            for (let i = 0; i < limit; i++) {
                 simulation.tick();
-                if (i % TICK_BATCH === 0) {
+                if (i % batch === 0) {
+                    const delta = Date.now() - start;
                     self.postMessage({
                         type: 'tick',
                         nodes: nodes,
                         links: links,
+                        tick: i,
+                        time: delta,
                     });
                 }
             }
